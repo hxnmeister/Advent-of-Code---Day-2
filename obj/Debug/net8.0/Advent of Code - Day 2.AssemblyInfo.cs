718801c016b673c57b05117b0719d0a3b1be351d
Valerii <valeriy.stronskiy@gmail.com>
2024-12-06 14:21:27 +0200

    gitignore

--- obj/Debug/net8.0/Advent of Code - Day 2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Advent of Code - Day 2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Advent of Code - Day 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9613727cd9d951c4519942f7f6fb7aa6fd2d291b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa957812012f95f98d49224cdfa75507540345f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Advent of Code - Day 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Advent of Code - Day 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
